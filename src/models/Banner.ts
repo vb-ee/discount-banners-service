c443bf8af753c3fc5a18b2297a8a4e8d2cdf21a3
vb1120 <bekmuradovimprove@gmail.com>
2022-08-01 18:26:22 +0500

    Banner model complete

---
+++ src/models/Banner.ts
@@ -0,0 +1,16 @@
+import { Schema, model } from 'mongoose'
+
+// Create an interface representing a document in MongoDB.
+export interface IBanner {
+    title: string
+    imageUrl: string
+}
+
+// Create a Schema corresponding to the document interfaces.
+export const bannerSchema = new Schema<IBanner>({
+    title: { type: String, required: true },
+    imageUrl: { type: String, required: true }
+})
+
+// Create a Model.
+export const Banner = model<IBanner>('Banner', bannerSchema)
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,8 @@
+import { initConnection } from './db/connection'
 import { startApp } from './startApp'
 
-startApp()
+initConnection('mongodb://banners-service-db/bannersdb')
+    .then(() => {
+        startApp()
+    })
+    .catch((err) => console.log(err.message))
