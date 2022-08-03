38297980df5e61c3103a483014d90380f0a62a6e
vb1120 <bekmuradovimprove@gmail.com>
2022-08-03 14:33:05 +0500

    banner image remove logic move to model instance method

--- src/models/Banner.ts
+++ src/models/Banner.ts
@@ -1,16 +1,28 @@
-import { Schema, model } from 'mongoose'
-
+import { Schema, model, Model } from 'mongoose'
+import fs from 'fs'
 // Create an interface representing a document in MongoDB.
 export interface IBanner {
     title: string
     imageUrl: string
 }
 
+export interface IBannerMethods {
+    removeImage(): void
+}
+
+type BannerModel = Model<IBanner, {}, IBannerMethods>
+
 // Create a Schema corresponding to the document interfaces.
-export const bannerSchema = new Schema<IBanner>({
+export const bannerSchema = new Schema<IBanner, BannerModel, IBannerMethods>({
     title: { type: String, required: true },
     imageUrl: { type: String, required: true }
 })
 
+bannerSchema.methods.removeImage = function () {
+    fs.unlink(this.imageUrl, (err) => {
+        console.log(err)
+    })
+}
+
 // Create a Model.
-export const Banner = model<IBanner>('Banner', bannerSchema)
+export const Banner = model<IBanner, BannerModel>('Banner', bannerSchema)
